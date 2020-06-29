b205472e0c1b672b81112c3dcc077dd9473be6ac
Alfredo Navas <elpuas@gmail.com>
2020-06-29 10:14:49 -0600

    Initial API Call

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+
+import Form from './components/Form';
+import Song from './components/Song';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	// initial state
+	// Search starts as an empty object
+  const [searchLyric, saveLyric] = useState({});
+
+  // Results starts as an empty string
+  const [lyric, savelyric] = useState('')
+
+
+	useEffect(() => {
+		// Return early and often.
+		if (Object.keys(searchLyric).length === 0) return;
+
+		// Create a Async/Await function to fetch API
+		const searchApiLyric = async () => {
+
+			const { artist, song } = searchLyric;
+
+			const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+			const res = await axios(url);
+
+			savelyric(res.data.lyrics);
+		};
+		// Invoke the function
+    searchApiLyric();
+
+	}, [searchLyric]); // Check any change on state
+
+	return (
+		<Fragment>
+			<Form saveLyric={saveLyric} />
+      <div>
+        <div></div>
+        <div>
+          <Song
+            lyric={lyric}
+          />
+        </div>
+      </div>
+		</Fragment>
+	);
 }
 
 export default App;
