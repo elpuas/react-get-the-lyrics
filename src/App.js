8a1479b731bfdd8b41e7f88fcb0a7f7ea9f1ac9c
Alfredo Navas <elpuas@gmail.com>
2020-06-29 20:37:43 -0600

    spotify-api-call

--- src/App.js
+++ src/App.js
@@ -11,12 +11,10 @@ function App() {
 	const [searchLyric, saveLyric] = useState({});
 
 	// Results starts as an empty string
-	const [lyric, saveTheLyric] = useState('')
+	const [lyric, saveTheLyric] = useState('');
 
 	// Spotify ID start as an empty array
-	const [spotid, saveSpotId] = useState('')
-
-
+	const [spotid, saveSpotId] = useState('');
 
 	useEffect(() => {
 		// Return early and often.
@@ -24,38 +22,37 @@ function App() {
 
 		// Create a Async/Await function to fetch API
 		const searchApiLyric = async () => {
-
 			// const id = '2dQd2qrFB5VtuUMp45G41p'
 			const { artist, song } = searchLyric;
 			const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
 			const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artist}`;
 			const url3 = {
-				"url": `https://api.spotify.com/v1/search?q=track%3A${song}%20artist%3A${artist}&type=track%2Cartist&limit=1`,
-				"method": "get",
-				"headers": {
-					"Accept": "application/json",
-					"Content-Type": "application/json",
-					"Authorization": "Bearer BQC9wAarLDylI9gkBAuOEDknhltDB7Z949_YG-eK2NPkzRF_7aT9ZerXpnulBqkPO3PvJmTGnMiVLYmt-CEqXKAQa1xi4RXI-4fj2L_WUB2JWKM7r9CqwB_xQo4mpo7eO0bD7wyIditMcIdnYyc"
+				url: `https://api.spotify.com/v1/search?q=track%3A${song}%20artist%3A${artist}&type=track%2Cartist&limit=1`,
+				method: 'get',
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					Authorization:
+						'Bearer BQC9wAarLDylI9gkBAuOEDknhltDB7Z949_YG-eK2NPkzRF_7aT9ZerXpnulBqkPO3PvJmTGnMiVLYmt-CEqXKAQa1xi4RXI-4fj2L_WUB2JWKM7r9CqwB_xQo4mpo7eO0bD7wyIditMcIdnYyc',
 				},
-			}
+			};
 
 			const [lyric, info, track] = await Promise.all([
 				axios(url),
 				axios(url2),
-				axios(url3)
+				axios(url3),
 				// axios(url3)
-			])
+			]);
 
-			console.log('INFO', info.data.artists[0].strStyle)
-			console.log('LETRA', lyric.data.lyrics)
-			console.log('SPOTIFY ID', track.data.tracks.items[0].id)
+			console.log('INFO', info.data.artists[0].strStyle);
+			console.log('LETRA', lyric.data.lyrics);
+			console.log('SPOTIFY ID', track.data.tracks.items[0].id);
 
 			saveTheLyric(lyric.data.lyrics);
-			saveSpotId(track.data.tracks.items[0].id)
+			saveSpotId(track.data.tracks.items[0].id);
 		};
 		// Invoke the function
 		searchApiLyric();
-
 	}, [searchLyric]); // Check any change on state
 
 	return (
@@ -63,11 +60,9 @@ function App() {
 			<Form saveLyric={saveLyric} />
 			<div>
 				<div>
-					<Spotify spotid={spotid}/>
+					<Spotify spotid={spotid} />
 
-					<Song
-						lyric={lyric}
-					/>
+					<Song lyric={lyric} />
 				</div>
 			</div>
 		</Fragment>
